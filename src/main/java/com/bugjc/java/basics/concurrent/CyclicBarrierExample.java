cd88cbde40f9edb87b7f620d7c8c6483fd581d7f
qingyang <qing.muyi@foxmail.com>
2018-08-13 17:25:31 +0800

    整理集成示例，升级spring boot版本。

--- src/main/java/com/bugjc/java/basics/concurrent/CyclicBarrierExample.java
+++ src/main/java/com/bugjc/java/basics/concurrent/CyclicBarrierExample.java
@@ -1,41 +1,48 @@
 package com.bugjc.java.basics.concurrent;
 
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.concurrent.*;
 
 public class CyclicBarrierExample {
+    /**
+     * Common Thread Pool
+     */
+    private static ExecutorService pool = new ThreadPoolExecutor(5, 200,
+            0L, TimeUnit.MILLISECONDS,
+            new LinkedBlockingQueue<Runnable>(1024), new ThreadFactoryBuilder().setNameFormat("demo-pool-%d").build(), new ThreadPoolExecutor.AbortPolicy());
+
+
+
 
     public static void main(String[] args) throws InterruptedException {
         int count = 1000;
-        //example(count);
+        example(count);
 
-        example1(count,new BaseTask());
+        //example1(count,new BaseTask());
     }
 
     public static void example(int count) throws InterruptedException {
         CyclicBarrier cyclicBarrier = new CyclicBarrier(count);
-        ExecutorService executorService = Executors.newFixedThreadPool(count);
         for (int i = 0; i < count; i++) {
-            executorService.execute(new OrderTask(cyclicBarrier));
+            pool.execute(new OrderTask(cyclicBarrier));
         }
-        executorService.shutdown();
+        pool.shutdown();
 
-        while (!executorService.isTerminated()){
+        while (!pool.isTerminated()){
             Thread.sleep(10);
             System.out.println("任务暂停10毫秒");
         }
     }
 
-    public static void example1(int count,Runnable runnable) throws InterruptedException {
+    private static void example1(int count, Runnable runnable) throws InterruptedException {
         CyclicBarrier cyclicBarrier = new CyclicBarrier(count,runnable);
-        ExecutorService executorService = Executors.newFixedThreadPool(count);
         for (int i = 0; i < count; i++) {
-            executorService.execute(new OrderTask(cyclicBarrier));
+            pool.execute(new OrderTask(cyclicBarrier));
         }
-        executorService.shutdown();
+        pool.shutdown();
 
-        while (!executorService.isTerminated()){
+        while (!pool.isTerminated()){
             Thread.sleep(10);
             System.out.println("任务暂停10毫秒");
         }
