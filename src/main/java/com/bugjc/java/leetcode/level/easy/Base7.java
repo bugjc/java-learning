4bd63129a91249598673c6e520e7beeaa8253f10
qingyang <qing.muyi@foxmail.com>
2018-09-19 15:15:09 +0800

    日常更新

---
+++ src/main/java/com/bugjc/java/leetcode/level/easy/Base7.java
@@ -0,0 +1,55 @@
+package com.bugjc.java.leetcode.level.easy;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 七进制数
+ * @author qingyang
+ * @date 2018/9/17 17:05
+ */
+@Slf4j
+public class Base7 {
+
+
+    public String convertToBase7(int num) {
+        //1:正整数、0:0、-1:负整数
+        int type = 0;
+        if (num < 0){
+            type = -1;
+            num = Math.abs(num);
+        }else if (num > 0){
+            type = 1;
+        }else {
+            return String.valueOf(type);
+        }
+        int value = num;
+        StringBuilder result = new StringBuilder();
+        while (value != 0){
+            //取余数
+            num = num % 7;
+            result.append(num);
+            //获取底数
+            value = value / 7;
+            num = value;
+        }
+        if (type == -1){
+            result.append("-");
+        }
+        return result.reverse().toString();
+    }
+
+    /**
+     * 就是除以7取馀数.
+     * 例如
+     * 100 = 14*7 +2 个位
+     * 14 = 2*7 +0 百位
+     * 2 = 0*7 + 2 千位
+     * (2753) | 10 = ( 11012) | 7
+     * @param args
+     */
+    public static void main(String[] args) {
+        String result = new Base7().convertToBase7(-7);
+        log.info(result);
+    }
+
+}
---
+++ src/main/java/com/bugjc/java/leetcode/level/easy/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,39 @@
+package com.bugjc.java.leetcode.level.easy;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 买卖股票的最佳时机 i
+ * @author qingyang
+ * @date 2018/9/17 23:08
+ */
+@Slf4j
+public class BestTimeToBuyAndSellStock {
+
+    public int maxProfit(int[] prices) {
+
+        int max = 0;
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i+1; j < prices.length; j++) {
+                if (prices[i] < prices[j]){
+                    int temp = prices[j] - prices[i];
+                    //比较收益值
+                    if (temp > max){
+                        max = temp;
+                    }
+
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = new int[]{7,1,5,3,6,4};
+        //prices = new int[]{7,6,4,3,1};
+        prices = new int[]{0,0,1};
+        int max = new BestTimeToBuyAndSellStock().maxProfit(prices);
+        log.info(String.valueOf(max));
+    }
+
+}
