b2cfdc1368d2b23a8258b5a09ce16f7cebda2d16
qing.muyi@foxmail.com <qing.muyi@foxmail.com>
2018-12-17 17:57:49 +0800

    日常提交

--- src/main/java/com/bugjc/java/basics/DemoExample.java
+++ src/main/java/com/bugjc/java/basics/DemoExample.java
@@ -1,6 +1,9 @@
 package com.bugjc.java.basics;
 
+import cn.hutool.core.util.RandomUtil;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.*;
 
 /**
  * @author qingyang
@@ -21,5 +24,32 @@ public class DemoExample {
         int n = new DemoExample().f(9);
         log.info(n + "");
 
+
+        int a = 300;
+        Map<String,Object> hash = new HashMap<>();
+        Map<String, List<Integer>> bucket = new HashMap<>();
+        for (int i = 0; i < a; i++) {
+            int max = RandomUtil.randomInt(0,5000);
+            hash.put(i+"",max);
+            int b = (i + max) % 10;
+            log.info("桶{}存放了值{}",b,i);
+            List<Integer> list = bucket.get(""+b);
+            if (list == null){
+                list = new ArrayList<>();
+            }
+            list.add(i);
+            bucket.put(""+b,list);
+        }
+
+        // 查询值80存放在哪个桶
+        int i = 80;
+        int max = Integer.parseInt(hash.get(""+i).toString());
+        int b = (i + max) % 10;
+        List<Integer> list = bucket.get(""+b);
+        for (int j = 0; j < list.size(); j++) {
+            if (list.get(j) != null && list.get(j) == i){
+                log.info("在桶{}，找到了值{}",b,i);
+            }
+        }
     }
 }
